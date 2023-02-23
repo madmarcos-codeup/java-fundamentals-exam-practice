313089ef9ae6d6bf4b8376e5fb1fe7cdaf5e6e84
Docrob <mark.robinson@codeup.com>
2023-02-23 13:22:04 -0600

    first commit

---
+++ src/main/java/TestGetVowelCountInColorMethod.java
@@ -0,0 +1,21 @@
+public class TestGetVowelCountInColorMethod {
+
+    public static void main(String[] args) {
+
+//        Cat blackCat = new Cat(4, true, "Midnight", "black");
+//        Cat orangeCat = new Cat(2, true, "Dreamsicle", "ORaNGE");
+//        Cat whiteCat = new Cat(10, false, "Snowball", "whIte");
+//
+//        System.out.println(blackCat.getVowelCountInColor()); // blackCat's color is black
+//        System.out.println(whiteCat.getVowelCountInColor()); // whiteCat's color is ORaNGE
+//        System.out.println(orangeCat.getVowelCountInColor()); // orangeCat's color is whIte
+
+        // DONT UNCOMMENT THIS
+        // OUTPUT:
+        // 1
+        // 3
+        // 2
+
+
+    }
+}
